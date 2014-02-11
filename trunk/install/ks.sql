2f28dbb43f7c1e6ae515427fb3c45d42767696b1
pbtwnet@gmail.com <pbtwnet@gmail.com@0d97b978-19ce-4060-5900-02fdddd32bd9>
2014-02-11 12:31:30 +0000

    變動sql的編碼設定

--- trunk/install/ks.sql
+++ trunk/install/ks.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 建立日期: 2014 �?02 ??11 ??13:01
--- 伺服器版本: 5.6.14
--- PHP 版本: 5.5.6
+-- 建立日期: 2014 年 02 月 11 日 13:30
+-- 伺服器版本: 5.5.34
+-- PHP 版本: 5.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,15 +27,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ks_sessions` (
-  `session_id` char(36) COLLATE utf8_bin NOT NULL,
+  `session_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `session_user_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `session_start` int(11) unsigned NOT NULL,
   `session_last_time` int(11) unsigned NOT NULL DEFAULT '0',
-  `session_ip` varchar(40) COLLATE utf8_bin NOT NULL,
-  `session_page` varchar(255) COLLATE utf8_bin NOT NULL,
+  `session_ip` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `session_page` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`session_id`),
   KEY `session_user_id` (`session_user_id`,`session_last_time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- 資料表的匯出資料 `ks_sessions`
@@ -51,14 +51,14 @@ INSERT INTO `ks_sessions` (`session_id`, `session_user_id`, `session_start`, `se
 --
 
 CREATE TABLE IF NOT EXISTS `ks_sessions_keys` (
-  `key_id` char(36) COLLATE utf8_bin NOT NULL,
+  `key_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `user_id` mediumint(8) unsigned NOT NULL,
   `add_time` int(11) unsigned NOT NULL DEFAULT '0',
   `last_login` int(11) unsigned NOT NULL DEFAULT '0',
-  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
+  `last_ip` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`key_id`,`user_id`),
   KEY `last_login` (`last_login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -68,16 +68,16 @@ CREATE TABLE IF NOT EXISTS `ks_sessions_keys` (
 
 CREATE TABLE IF NOT EXISTS `ks_users` (
   `user_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `user_enable` enum('N','Y') COLLATE utf8_bin NOT NULL DEFAULT 'N',
-  `user_is_admin` enum('N','Y') COLLATE utf8_bin NOT NULL DEFAULT 'N',
-  `user_name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `user_password` varchar(32) COLLATE utf8_bin NOT NULL,
-  `user_email` varchar(100) COLLATE utf8_bin NOT NULL,
+  `user_enable` enum('N','Y') CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'N',
+  `user_is_admin` enum('N','Y') CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'N',
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `user_password` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `user_email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `user_regdate` int(11) unsigned NOT NULL DEFAULT '0',
   `last_login` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
 -- 資料表的匯出資料 `ks_users`
