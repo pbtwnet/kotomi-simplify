618000834d86820b0859a3051fd41a5780338d2b
pbtwnet@gmail.com <pbtwnet@gmail.com@0d97b978-19ce-4060-5900-02fdddd32bd9>
2014-02-12 06:01:19 +0000

    稍微變動資料表欄位，所以輸出過新的sql

--- trunk/install/ks.sql
+++ trunk/install/ks.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- 主機: 127.0.0.1
--- 建立日期: 2014 年 02 月 11 日 13:30
--- 伺服器版本: 5.5.34
--- PHP 版本: 5.4.22
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- 資料庫: `ks`
---
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `ks_sessions`
---
 
 CREATE TABLE IF NOT EXISTS `ks_sessions` (
   `session_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -37,18 +18,8 @@ CREATE TABLE IF NOT EXISTS `ks_sessions` (
   KEY `session_user_id` (`session_user_id`,`session_last_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- 資料表的匯出資料 `ks_sessions`
---
-
 INSERT INTO `ks_sessions` (`session_id`, `session_user_id`, `session_start`, `session_last_time`, `session_ip`, `session_page`) VALUES
-('D7D1DDF3-B363-43CA-8C44-F1FA4DCF35E1', 1, 1392112943, 1392112946, '127.0.0.1', '/Kotomi-Simplify/index.php');
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `ks_sessions_keys`
---
+('20EE9E77-26F7-4C4F-6295-B15D6EB9979A', 1, 1392183859, 1392183897, '127.0.0.1', '/Kotomi-Simplify/index.php');
 
 CREATE TABLE IF NOT EXISTS `ks_sessions_keys` (
   `key_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -60,31 +31,20 @@ CREATE TABLE IF NOT EXISTS `ks_sessions_keys` (
   KEY `last_login` (`last_login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- 資料表結構 `ks_users`
---
-
 CREATE TABLE IF NOT EXISTS `ks_users` (
   `user_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `user_enable` enum('N','Y') CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'N',
-  `user_is_admin` enum('N','Y') CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'N',
+  `user_level` tinyint(2) unsigned NOT NULL DEFAULT '0',
   `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `user_password` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `user_email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `user_regdate` int(11) unsigned NOT NULL DEFAULT '0',
   `last_login` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
---
--- 資料表的匯出資料 `ks_users`
---
-
-INSERT INTO `ks_users` (`user_id`, `user_enable`, `user_is_admin`, `user_name`, `user_password`, `user_email`, `user_regdate`, `last_login`) VALUES
-(1, 'Y', 'N', 'anonymous', '', '', 0, 0);
+INSERT INTO `ks_users` (`user_id`, `user_enable`, `user_level`, `user_name`, `user_password`, `user_regdate`, `last_login`) VALUES
+(1, 'Y', 0, 'anonymous', '', 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
